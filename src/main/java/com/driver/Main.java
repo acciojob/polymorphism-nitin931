c47181f405c0af065b2c3e6c863803c70521ce9b
nitin931 <nitinthakur931@gmail.com>
2023-08-18 16:02:07 +0530

    Attempt 1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,12 @@ package com.driver;
 
 public class Main {
 
+    Product p =new Product();
+
+    int a = p.product(10,2);
+    int b = p.product(10,2,3);
+    double x = 10, y =2;
+    double c = p.product(x,y);
+
+
 }
